b2310491fb2e8a9c59b7b8d0003cf4c1eb75fbad
elidallen <mitchellallen2@yahoo.com>
2023-11-29 18:49:25 -0500

    first commit

---
+++ public/pizza/data.jsx
@@ -0,0 +1,28 @@
+// data/pizzaData.js
+
+const pizzaData = [
+    {
+      id: 1,
+      name: 'Margherita',
+      toppings: ['Tomato Sauce', 'Mozzarella', 'Basil'],
+      size: 'Medium',
+      price: 10.99,
+    },
+    {
+      id: 2,
+      name: 'Pepperoni',
+      toppings: ['Tomato Sauce', 'Mozzarella', 'Pepperoni'],
+      size: 'Large',
+      price: 12.99,
+    },
+    {
+      id: 3,
+      name: 'Vegetarian',
+      toppings: ['Tomato Sauce', 'Mozzarella', 'Mushrooms', 'Bell Peppers', 'Onions', 'Olives'],
+      size: 'Medium',
+      price: 11.99,
+    },
+    // Add more pizzas as needed
+  ];
+  
+  export default pizzaData;
